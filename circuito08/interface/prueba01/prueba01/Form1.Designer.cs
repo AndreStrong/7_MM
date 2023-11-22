db9cad6e09a94be2416228c42e4085d99f89e6be
andres_garcia <andygarciac5@gmail.com>
2023-11-22 00:27:18 -0600

    se agrega el circuito 08

---
+++ circuito08/interface/prueba01/prueba01/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace prueba01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cambiarColor = new System.Windows.Forms.GroupBox();
+            this.imprimir = new System.Windows.Forms.Label();
+            this.cambiar = new System.Windows.Forms.Button();
+            this.cambiarColor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cambiarColor
+            // 
+            this.cambiarColor.Controls.Add(this.imprimir);
+            this.cambiarColor.Controls.Add(this.cambiar);
+            this.cambiarColor.Location = new System.Drawing.Point(97, 31);
+            this.cambiarColor.Name = "cambiarColor";
+            this.cambiarColor.Size = new System.Drawing.Size(533, 374);
+            this.cambiarColor.TabIndex = 0;
+            this.cambiarColor.TabStop = false;
+            this.cambiarColor.Text = "Color";
+            this.cambiarColor.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // imprimir
+            // 
+            this.imprimir.AutoSize = true;
+            this.imprimir.Location = new System.Drawing.Point(224, 69);
+            this.imprimir.Name = "imprimir";
+            this.imprimir.Size = new System.Drawing.Size(46, 20);
+            this.imprimir.TabIndex = 1;
+            this.imprimir.Text = "Hola!";
+            this.imprimir.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cambiar
+            // 
+            this.cambiar.Location = new System.Drawing.Point(208, 165);
+            this.cambiar.Name = "cambiar";
+            this.cambiar.Size = new System.Drawing.Size(117, 50);
+            this.cambiar.TabIndex = 0;
+            this.cambiar.Text = "Cambiar!";
+            this.cambiar.UseVisualStyleBackColor = true;
+            this.cambiar.Click += new System.EventHandler(this.cambiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cambiarColor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.cambiarColor.ResumeLayout(false);
+            this.cambiarColor.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox cambiarColor;
+        private System.Windows.Forms.Button cambiar;
+        private System.Windows.Forms.Label imprimir;
+    }
+}
+
---
+++ circuito08/interface/prueba01/prueba01/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prueba01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+              
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cambiar_Click(object sender, EventArgs e)
+        {
+            int contadorCero = 0;
+            int contadorUno = contadorCero + 1;
+            if (contadorUno == 1)
+            {
+                imprimir.Text = "Yo soy";
+                contadorUno = contadorCero + 1;
+            }
+            else if (contadorUno == 2)
+            {
+                imprimir.Text = "Una figura";
+                contadorUno = contadorCero + 1;
+            }
+            else if (contadorUno == 3)
+            {
+                imprimir.Text = "Lalala...";
+                contadorUno = contadorCero + 1;
+            }
+            else if(contadorUno > 3) {
+                contadorUno = 0;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
